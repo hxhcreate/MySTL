882201cecadb9951dd4251bc80caa4f08b14779d
hxhcreate <hxh_create@163.com>
2022-03-14 16:14:41 +0800

    1

---
+++ allocator.h
@@ -0,0 +1,68 @@
+//
+// Created by HXH on 2022/3/14.
+//
+#ifndef MYSTL_ALLOCATOR_H
+#define MYSTL_ALLOCATOR_H
+
+#include <cstddef>
+#include "construct.h"
+namespace mystl
+{
+
+template <class T>
+class allocator
+{
+public:
+    typedef T         value_type;
+    typedef T*        pointer;
+    typedef const T   const_pointer;
+    typedef T&        reference;
+    typedef size_t    size_type;
+    typedef ptrdiff_t  difference_type;
+
+public:
+    static pointer allocate();
+    static pointer  allocate(size_type n);
+
+    static void deallocate(T* ptr, size_type n = 0);  //修改成一个有默认参数的函数 , n is unused
+
+    static void construct(T* ptr);
+    static void construct(T* ptr, const T&value);
+//    static void construct(pointer ptr, reference& value);  move函数暂时不能理解
+};
+
+template<class T>
+static T* allocator<T>::allocate(){
+    return static_cast<T*>(::operator new(sizeof(T)))
+}
+template<class T>
+static T* allocator<T>::allocate(size_type n){
+    if(n==0)
+        return nullptr;
+    return static_cast<pointer>(::operator new (n * sizeof(T)))
+}
+
+template<class T>
+void allocator<T>::deallocate(T* ptr, size_type n) {
+    if(ptr == nullptr)
+        return;
+    ::operator delete(ptr;)
+}
+
+template<class T>
+void allocator<T>::construct(T* ptr) {
+    mystl::construct(ptr);
+}
+
+template<class T>
+void allocator<T>::construct(T* ptr, const T &value) {
+    mystl::construct(ptr, value)
+}
+
+//template<class T>
+//void allocator<T>::construct(allocator::pointer ptr, allocator::reference &value){
+//    mystl::construct(ptr, mystl::move(value));
+}
+
+
+#endif //MYSTL_ALLOCATOR_H
